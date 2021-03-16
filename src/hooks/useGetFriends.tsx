40386a07743e89d6508143c7d248bcc793f48f85
ayushupdh <aayushupadhyay14@gmail.com>
2021-03-16 16:41:33 -0500

    Created custom hook to get friends list

---
+++ src/hooks/useGetFriends.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { setAuthToken } from "../utils/authToken";
+
+import { server } from "../api/server";
+
+export const useGetFriends = () => {
+  const [friends, setFriends] = useState<any>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const token = await AsyncStorage.getItem("userToken");
+        setAuthToken(token);
+        const response = await server.get("/users/me/friend");
+        const friensList = response.data.friends;
+        // const groupNames = response.data.groups.map((group: any) => {
+        //   return {
+        //     name: group.name,
+        //     id: group._id,
+        //   };
+        // });
+        setFriends(friensList);
+        setError(null);
+      } catch (e) {
+        setError(e.message);
+      }
+    })();
+  }, []);
+  return { friends, error };
+};
