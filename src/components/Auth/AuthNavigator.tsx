9fa000655b8a5ceca5191b10a68936a92280ba8c
ayushupdh <aayushupadhyay14@gmail.com>
2021-03-16 15:35:23 -0500

    Removed unwanted dependencies

--- src/components/Auth/AuthNavigator.tsx
+++ src/components/Auth/AuthNavigator.tsx
@@ -1,16 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { ActivityIndicator, View } from "react-native";
 import Signin from "./Signin";
 import Signup from "./Signup";
 import { AuthParamList } from "./AuthTypes";
-import { useDispatch, useSelector } from "react-redux";
-import HomeScreen from "../HomeScreen";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { server } from "../../api/server";
-import { LOAD_USER } from "../../redux/types/Authtypes";
 
 type AuthNavigatorProps = {};
 
