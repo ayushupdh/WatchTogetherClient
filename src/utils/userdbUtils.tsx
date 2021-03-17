bb67203add098afd5bd916c12ca14293d42b7874
ayushupdh <aayushupadhyay14@gmail.com>
2021-03-17 00:11:51 -0500

    Moved user related server stuff to a new file

---
+++ src/utils/userdbUtils.tsx
@@ -0,0 +1,66 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { server } from "../api/server";
+import { setAuthToken } from "./authToken";
+
+export const addLikedMovie = async (movieId: string) => {
+  let error = null;
+  let response = null;
+  try {
+    const token = await AsyncStorage.getItem("userToken");
+    setAuthToken(token);
+    await server.post("/users/me/likedMovies", { movieId });
+    response = "OK";
+    return { response, error };
+  } catch (e) {
+    error = e;
+    return { response, error };
+  }
+};
+
+export const addDislikedMovie = async (movieId: string) => {
+  let error = null;
+  let response = null;
+  try {
+    const token = await AsyncStorage.getItem("userToken");
+    setAuthToken(token);
+    await server.post("/users/me/dislikedMovies", { movieId });
+    response = "OK";
+    return { response, error };
+  } catch (e) {
+    error = e;
+    return { response, error };
+  }
+};
+
+export const searchUsers = async (username: string) => {
+  let error = null;
+  let response = null;
+  try {
+    const token = await AsyncStorage.getItem("userToken");
+    setAuthToken(token);
+    const r = await server.get("/users/search", { params: { username } });
+    response = r.data;
+    return { response, error };
+  } catch (e) {
+    error = e.message;
+    return { response, error };
+  }
+};
+
+export const addFriend = async (username: string) => {
+  let error = null;
+  let response = null;
+  try {
+    const token = await AsyncStorage.getItem("userToken");
+    setAuthToken(token);
+    const r = await server.patch("/users/me/friend", {
+      friend: username,
+    });
+    response = "OK";
+    return { response, error };
+  } catch (e) {
+    error = e.message;
+    return { response, error };
+  }
+};
